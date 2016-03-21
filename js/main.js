e30117c1434ac1bd3321ad2d819ed3817e2282ef
Dawid <dawidjez@gmail.com>
2016-03-21 22:40:59 +0100

    update menu events

--- js/main.js
+++ js/main.js
@@ -2,18 +2,24 @@ $(document).ready(function () {
     $("#left-panel nav li").click(function () {
         $("#left-panel nav li").removeClass("current")
         $(this).addClass("current")
+
+        var title;
+
         switch ($(this).attr("id")) {
             case "all":
+                title = "All projects";
                 $(".project").show()
             break;
             case "games":
+                title = "Games";
                 $(".project").show().not(".game").hide()
             break;
             case "apps":
+                title = "Apps";
                 $(".project").show().not(".application").hide()
             break;
             default: return false;
-
         }
+        $("h2").text(title);
     })
 })
