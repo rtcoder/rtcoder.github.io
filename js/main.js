6c469db46d33c413e21d1befd2b1d6878fb64420
Dawid Jeż <dawidjez@gmail.com>
2016-11-23 01:58:22 +0100

    new layout

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 550);
+
+        event.preventDefault();
+    });
+
+	$(window).scroll(function(e){
+		if($(document).scrollTop() >= 150){
+			$('nav.navbar').addClass('fixed');
+		}else{
+			$('nav.navbar').removeClass('fixed');
+		}
+	});
+
+	$('body').scrollspy({
+        target: '.navbar-default',
+        offset: 51
+    });
+    
+    $('[data-toggle="tooltip"]').tooltip();
+
+});
