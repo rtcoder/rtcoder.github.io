13f70712be3cddef97378c7f6dceecf24c978225
Dawid Jeż <dawidjez@gmail.com>
2017-04-16 19:10:13 +0200

    less fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-	$('.page-scroll a').bind('click', function(event) {
+var GitHubUername = 'rtcoder';
+
+$(document).ready(function () {
+    $('.page-scroll a').bind('click', function (event) {
         var $anchor = $(this);
 
         $('html, body').stop().animate({
@@ -9,19 +11,26 @@ $(document).ready(function(){
         event.preventDefault();
     });
 
-	$(window).scroll(function(e){
-		if($(document).scrollTop() >= 150){
-			$('nav.navbar').addClass('fixed');
-		}else{
-			$('nav.navbar').removeClass('fixed');
-		}
-	});
+    $(window).scroll(function (e) {
+        if ($(document).scrollTop() >= 150) {
+            $('nav.navbar').addClass('fixed');
+        } else {
+            $('nav.navbar').removeClass('fixed');
+        }
+    });
 
-	$('body').scrollspy({
+    $('body').scrollspy({
         target: '.navbar-default',
         offset: 51
     });
-    
+
     $('[data-toggle="tooltip"]').tooltip();
 
 });
+
+var getGitHubUser = function () {
+    
+}
+var getGitHubUserRepos = function () {
+    
+}
