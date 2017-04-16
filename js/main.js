848806bc9194d22beaa6dde570404297fe1275b7
Dawid Jeż <dawidjez@gmail.com>
2017-04-16 20:21:59 +0200

    init new version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 var GitHubUername = 'rtcoder';
+var repos = null;
+var getGitHubUser = function () {
+    $.get('https://api.github.com/users/' + GitHubUername, function (data) {
+        return data;
+    });
+};
+var getGitHubUserRepos = function () {
+    $.get('https://api.github.com/users/' + GitHubUername + '/repos', function (data) {
+        repos = data;
+        $('.projects-list').find('i').remove();
+        for (i in repos) {
+            var icon = '<div class="col-xs-1"><i class="fa fa-github fa-2x"></i></div>';
+            var name = '<div class="col-xs-11"></div>';
+            $('.projects-list').append('<div class="col-xs-12 project">' + icon + '</div>')
+        }
+    });
+};
+getGitHubUserRepos();
 
 $(document).ready(function () {
+
     $('.page-scroll a').bind('click', function (event) {
         var $anchor = $(this);
 
@@ -25,12 +44,4 @@ $(document).ready(function () {
     });
 
     $('[data-toggle="tooltip"]').tooltip();
-
 });
-
-var getGitHubUser = function () {
-    
-}
-var getGitHubUserRepos = function () {
-    
-}
