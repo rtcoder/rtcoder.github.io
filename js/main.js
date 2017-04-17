934dbc1a319c7f3a042d8760de88ada42f5ed984
Dawid Jeż <dawidjez@gmail.com>
2017-04-17 02:11:07 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,72 @@
 var GitHubUername = 'rtcoder';
 var repos = null;
+var blacklist = ['rtcoder.github.io', 'etnel', 'js-os', 'WebWriter', 'phybel_strona', 'game', 'SocialZone'];
+var langColors = {
+    JavaScript: '#f1e05a',
+    CSS: '#563d7c',
+    PHP: '#4F5D95',
+    HTML: '#e34c26',
+    Batchfile: '#C1F12E',
+    PowerShell: '#012456',
+    ApacheConf: '',
+    
+};
 var getGitHubUser = function () {
     $.get('https://api.github.com/users/' + GitHubUername, function (data) {
         return data;
     });
 };
+var getGitHubProjectLanguages = function (name) {
+    $.get('https://api.github.com/repos/' + GitHubUername + '/' + name + '/languages', function (langs) {
+//        console.log(langs)
+        var sum = 0;
+        Object.keys(langs).forEach(function (key) {
+            sum += langs[key];
+        });
+        Object.keys(langs).forEach(function (key) {
+            var percent = ((langs[key] * 100) / sum).toFixed(1);
+            $('[data-name="' + name + '"]').find('.langs').append('<div class="lang" style="background:' + langColors[key] + ';width:' + percent + '%"></div>')
+        });
+    });
+};
+
 var getGitHubUserRepos = function () {
     $.get('https://api.github.com/users/' + GitHubUername + '/repos', function (data) {
         repos = data;
         $('.projects-list').find('i').remove();
         for (i in repos) {
-            var icon = '<div class="col-xs-1"><i class="fa fa-github fa-2x"></i></div>';
-            var name = '<div class="col-xs-11"></div>';
-            $('.projects-list').append('<div class="col-xs-12 project">' + icon + '</div>')
+            if (blacklist.indexOf(repos[i].name) >= 0) {
+                continue;
+            }
+
+
+            var stars = repos[i].stargazers_count > 0 ? '<span class="stars pull-right"><i class="fa fa-star"></i> ' + repos[i].stargazers_count + '</span>' : '';
+
+            var description = repos[i].description ? repos[i].description : '';
+            var repo = '<div class="col-xs-12 project" data-name="' + repos[i].name + '">' +
+                    '<div class="row">' +
+                    '<div class="col-sm-6 col-md-12 name">' +
+                    '<a href="' + repos[i].html_url + '">'
+                    + repos[i].name +
+                    '</a>' + stars +
+                    '</div>' +
+                    '<div class="col-sm-6 col-md-12 description">'
+                    + description +
+                    '</div>' +
+                    '</div>' +
+                    '<div class="langs"></div>'
+            '</div>';
+
+
+            $('.projects-list').append(repo);
+            getGitHubProjectLanguages(repos[i].name);
+
         }
     });
 };
 getGitHubUserRepos();
 
 $(document).ready(function () {
-
-    $('.page-scroll a').bind('click', function (event) {
-        var $anchor = $(this);
-
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 550);
-
-        event.preventDefault();
-    });
-
-    $(window).scroll(function (e) {
-        if ($(document).scrollTop() >= 150) {
-            $('nav.navbar').addClass('fixed');
-        } else {
-            $('nav.navbar').removeClass('fixed');
-        }
-    });
-
-    $('body').scrollspy({
-        target: '.navbar-default',
-        offset: 51
-    });
 
     $('[data-toggle="tooltip"]').tooltip();
 });
