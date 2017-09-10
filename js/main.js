986dd7757b53ef62f7ecdfc92d6f046054499a62
Dawid Jeż <dawidjez@gmail.com>
2017-09-10 18:43:18 +0200

    update

--- js/main.js
+++ js/main.js
@@ -14,25 +14,14 @@ var skillsList = [
     {title: "MySQL", classname: "devicon-mysql-plain-wordmark"},
     {title: "Git", classname: "devicon-git-plain"}
 ];
-var getGitHubGists = function () {
-    $.get('https://api.github.com/users/' + GitHubUername, function (data) {
-        $('#home .image').html('<img class="img-responsive" src="' + data.avatar_url + '">');
-        $('#home h1#username').text(data.name);
-        $('#home #bio').html(data.bio);
-        $('#home #location').html('<i class="fa fa-map-marker"></i> ' + data.location);
-        if (data.hireable) {
-            $('#home #hire').html('<i class="fa fa-check"></i> Avalible to hire');
-        }
-    });
-};
 var getGitHubUser = function () {
     $.get('https://api.github.com/users/' + GitHubUername, function (data) {
         $('#home .image').html('<img class="img-responsive" src="' + data.avatar_url + '">');
         $('#home h1#username').text(data.name);
         $('#home #bio').html(data.bio);
         $('#home #location').html('<i class="fa fa-map-marker"></i> ' + data.location);
-        if (data.hireable) {
-            $('#home #hire').html('<i class="fa fa-check"></i> Avalible to hire');
+        if (!data.hireable) {
+            $('#home #hire').remove();
         }
     });
 };
@@ -46,27 +35,30 @@ var getGitHubUserRepos = function () {
             var description = data[i].description ? data[i].description : '';
 
             var repo = '<div class="col-xs-12 project" data-name="' + data[i].name + '">' +
-                        '<div class="row">' +
-                            '<div class="col-xs-12 name">' +
-                                '<a href="' + data[i].html_url + '">'
-                                    + data[i].name +
-                                '</a>'
-                                + page 
-                                + stars +
-                            '</div>' +
-                            '<div class="col-xs-12 description">'
-                                + description +
-                            '</div>' +
-                        '</div>' +
+                    '<div class="row">' +
+                    '<div class="col-xs-12 name">' +
+                    '<a href="' + data[i].html_url + '">'
+                    + data[i].name +
+                    '</a>'
+                    + page
+                    + stars +
+                    '</div>' +
+                    '<div class="col-xs-12 description">'
+                    + description +
+                    '</div>' +
+                    '</div>' +
                     '</div>';
             $('.projects-list').append(repo);
         }
     });
 };
-getGitHubUserRepos();
-getGitHubUser();
-$(document).ready(function () {
+var addSkills = function () {
     for (var skill in skillsList) {
         $('#skills .row').append('<div class="col-lg-3 col-sm-3 col-xs-4 text-center skill-div"><i class="' + skillsList[skill].classname + ' colored" title="' + skillsList[skill].title + '"></i></div>');
     }
+};
+getGitHubUserRepos();
+getGitHubUser();
+$(document).ready(function () {
+    addSkills();
 });
