2a73205f028b36209a03009ed3e21fbed6aacea0
Dawid Jeż <dawidjez@gmail.com>
2018-09-20 23:42:25 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 var GitHubUername = 'rtcoder';
 var repos = null;
 
-var skillsList = [
-    {title: "HTML5", classname: "devicon-html5-plain"},
-    {title: "CSS3", classname: "devicon-css3-plain"},
-    {title: "LESS", classname: "devicon-less-plain-wordmark"},
-    {title: "Bootstrap", classname: "devicon-bootstrap-plain"},
-    {title: "Javascript", classname: "devicon-javascript-plain"},
-    {title: "jQuery", classname: "devicon-jquery-plain-wordmark"},
-    {title: "PHP", classname: "devicon-php-plain"},
-    {title: "Zend Framework", classname: "devicon-zend-plain"},
-    {title: "Laravel", classname: "devicon-laravel-plain"},
-    {title: "MySQL", classname: "devicon-mysql-plain-wordmark"},
-    {title: "Git", classname: "devicon-git-plain"}
-];
 var getGitHubUser = function () {
     $.get('https://api.github.com/users/' + GitHubUername, function (data) {
         $('#home .image').html('<img class="img-responsive" src="' + data.avatar_url + '">');
@@ -52,13 +39,6 @@ var getGitHubUserRepos = function () {
         }
     });
 };
-var addSkills = function () {
-    for (var skill in skillsList) {
-        $('#skills .row').append('<div class="col-lg-3 col-sm-3 col-xs-4 text-center skill-div"><i class="' + skillsList[skill].classname + ' colored" title="' + skillsList[skill].title + '"></i></div>');
-    }
-};
+
 getGitHubUserRepos();
 getGitHubUser();
-$(document).ready(function () {
-    addSkills();
-});
