ef5b077f5bf680f47605e5d908e240d1fa219dbd
Dawid Jeż <dawidjez@gmail.com>
2019-05-19 10:33:24 +0200

    new version

--- js/app.js
+++ js/app.js
@@ -1,83 +1,40 @@
 new Vue({
     el: '#app',
     data: () => ({
-        view: 'card',
         gitHubUername: 'rtcoder',
         apiUrl: 'https://api.github.com/',
-        userData: null,
+        userData: {},
         repos: [],
-        windowWidth: 0,
-        columnsCount: 3
+        gists: [],
+        columnsCount: 3,
+        langColors: {}
     }),
     methods: {
         getUser() {
-            const url = this.apiUrl + 'users/' + this.gitHubUername;
-            this.$http.get(url).then(response => {
-                this.userData = response.data;
-            }, error => {
-                console.log(error.statusText);
-            });
+            this.$http.get(`${this.apiUrl}users/${this.gitHubUername}`)
+                .then(response => this.userData = response.data
+                    , error => console.log(error.statusText));
         },
         getRepos() {
-            const url = this.apiUrl + 'users/' + this.gitHubUername + '/repos';
-            this.$http.get(url).then(response => {
-                this.repos = this.chunkArray(response.data, response.data.length / this.columnsCount);
-                this.changeColumns();
-            }, error => {
-                console.log(error.statusText);
-            });
+            this.$http.get(`${this.apiUrl}users/${this.gitHubUername}/repos`)
+                .then(response => this.repos = response.data
+                    , error => console.log(error.statusText));
         },
-        chunkArray(arr, chunk_size) {
-            var arrayLength = arr.length;
-            var tempArray = [];
-
-            for (let index = 0; index < arrayLength; index += chunk_size) {
-                const myChunk = arr.slice(index, index + chunk_size);
-                tempArray.push(myChunk);
-            }
-
-            return tempArray;
+        getGists() {
+            this.$http.get(`${this.apiUrl}users/${this.gitHubUername}/gists`)
+                .then(response => console.log(response.data)
+                    , error => console.log(error.statusText));
         },
-        getWindowWidth(event) {
-            this.windowWidth = document.documentElement.clientWidth;
-            this.changeColumns();
-        },
-        changeColumns() {
-            if (!this.repos.length) {
-                return;
-            }
-
-            let columnsCount = this.columnsCount;
-
-            if (this.windowWidth >= 700) {
-                columnsCount = 3;
-            } else if (this.windowWidth < 700 && this.windowWidth >= 500) {
-                columnsCount = 2;
-            } else {
-                columnsCount = 1;
-            }
-
-            if (columnsCount === this.columnsCount) {
-                return;
-            }
-            this.columnsCount = columnsCount;
-            const tmp = [];
-            for (const arr of this.repos) {
-                for (const obj of arr) {
-                    tmp.push(obj);
-                }
-            }
-            this.repos = this.chunkArray(tmp, tmp.length / this.columnsCount);
-
+        getLangsColors() {
+            this.$http.get('https://gist.github.com/rtcoder/dec3feab629683d998398c961c1092f5.js')
+                .then(response => this.langColors = response.data
+                    , error => console.log(error.statusText));
         }
     },
     mounted(event) {
         this.getUser();
         this.getRepos();
-        this.getWindowWidth(event);
-        window.addEventListener('resize', this.getWindowWidth);
-    },
-    beforeDestroy() {
-        window.removeEventListener('resize', this.getWindowWidth);
+        this.getGists();
+        this.getLangsColors();
     }
 });
