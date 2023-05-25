0c24ca6325f0e80cebacede74870cf6abd9e4afd
Dawid Jeż <dawidjez@gmail.com>
2023-05-25 13:28:18 +0200

    new layut

---
+++ app.js
@@ -0,0 +1,20 @@
+document.querySelector('.tags').addEventListener('click', e => {
+    const {target} = e;
+    if (!target.classList.contains('tag')) {
+        return;
+    }
+    document.querySelectorAll('.tag').forEach(el => el.classList.remove('active'));
+    target.classList.add('active');
+    const tagCategory = target.getAttribute('data-tag-category');
+    if (tagCategory === 'all') {
+        document.querySelectorAll('.list-item').forEach(el => el.style.removeProperty('display'));
+        return;
+    }
+    document.querySelectorAll('.list-item').forEach(el => {
+        if (el.getAttribute('data-tag').includes(tagCategory)) {
+            el.style.removeProperty('display');
+        } else {
+            el.style.display = 'none';
+        }
+    });
+})
